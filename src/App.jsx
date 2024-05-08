57997813695739bbd18f7129e2314e13711f27e3
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-05-08 22:10:41 +0300

    validating after submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header.jsx";
-import Login from "./components/StateLogin.jsx";
-import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
