ab0ceaaf62691a7d7d75c6fd0e48eedf9681bb9e
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-05-08 20:45:40 +0300

    validate user input every key

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -9,10 +9,17 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
 
     console.log(enteredValues);
+    setEnteredValues({
+      email: "",
+      password: "",
+    });
   }
 
   function handleInputChange(identifier, value) {
@@ -42,6 +49,9 @@ export default function Login() {
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please, enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
